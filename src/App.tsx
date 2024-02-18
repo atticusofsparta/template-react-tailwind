52ba975a4efc2be54ab3782f8dc5971920d9e188
atticusofsparta <atticusofsparta@protonmail.com>
2024-02-17 19:23:57 -0600

    fix(linter): fix linter plugin conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,12 @@ import {
 
 import AppRouterLayout from './components/layout/AppRouterLayout';
 import NotFound from './pages/NotFound';
-import { useGlobalState } from './services/state/useGlobalState';
 
 const Home = React.lazy(() => import('./pages/Home'));
 
 const sentryCreateBrowserRouter = wrapCreateBrowserRouter(createBrowserRouter);
 
 function App() {
-  const { theme, setTheme } = useGlobalState();
-
   const router = sentryCreateBrowserRouter(
     createRoutesFromElements(
       <Route element={<AppRouterLayout />} errorElement={<NotFound />}>
